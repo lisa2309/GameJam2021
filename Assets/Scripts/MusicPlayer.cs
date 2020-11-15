9084273de08fee030180a7fd074ca1f1eae218a9
Ronja Haas <ronja.jamie.haas@mni.thm.de>
2020-11-15 10:04:54 +0100

    Prefabs,new lvl design

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,37 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour
+{
+
+    public AudioSource AudioSource;
+    private float musicVolume = 1f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        AudioSource.Play();   
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //leiser
+        if (Input.GetKey(KeyCode.O))
+        {
+            musicVolume -= 0.05f;
+        }
+        //lauter
+        if (Input.GetKey(KeyCode.P))
+        {
+            musicVolume += 0.05f; 
+        }
+        AudioSource.volume = musicVolume;
+    }
+
+    public void updateVolume(float volume)
+    {
+        musicVolume = volume;
+    }
+}
